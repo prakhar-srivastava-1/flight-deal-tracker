98790ac7e735d24fe7bcdc45ec23dc3fe7a44333
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-08-30 14:54:20 +0530

    add logic to get list of all users from Google Sheet

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 from data_manager import DataManager
 from user_data import UserData
 
-user = UserData()
-user.get_details()
+# user = UserData()
+# user.get_details()
 
 dm = DataManager()
 dm.initialise_service()
-dm.add_user(user)
+# dm.add_user(user)
+
+# read all records from "users"
+users = dm.read_records(sheet_name="users")
+
+# create user objects and send email
+user_list = list()
+
+for user in users:
+    user = UserData(
+        first_name=user[0],
+        last_name=user[1],
+        email=user[2]
+    )
+    user_list.append(user)
+
+# send email
+
+print(user_list)
