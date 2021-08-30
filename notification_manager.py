231edbfefa97f35b9dd6a711a878c464f2b8013b
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-08-30 07:37:17 +0530

    add SMS logic using Twilio API

--- notification_manager.py
+++ notification_manager.py
@@ -1,3 +1,25 @@
+from secrets import TWILIO_PHONE, TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN, MY_PHONE
+from twilio.rest import Client
+from flight_data import FlightData
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+
+    def __init__(self):
+        self.account_sid = TWILIO_ACCOUNT_SID
+        self.phone = TWILIO_PHONE
+        self.auth_token = TWILIO_AUTH_TOKEN
+        self.my_phone = MY_PHONE
+        self.client = Client(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN)
+
+    def send_alert(self, flight: FlightData):
+        text_body = f"Only £{flight.price} to fly from " \
+                    f"{flight.departure_city}-{flight.departure_airport_code} to " \
+                    f"{flight.departure_city}-{flight.departure_airport_code}, from " \
+                    f"{flight.departure_date} to {flight.ret_dep_date}."
+        message = self.client.messages.create(
+            body=text_body,
+            from_=self.phone,
+            to=self.my_phone
+        )
+        print(message.status)
