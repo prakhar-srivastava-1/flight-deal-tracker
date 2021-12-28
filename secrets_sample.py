2958e465f005bf8f6c6279ccb9a689b3e10a545e
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2021-12-28 06:28:11 +0530

    correct spellings secrets_sample.py

--- secrets_sample.py
+++ secrets_sample.py
@@ -6,9 +6,8 @@ SPREADSHEET_ID = "YOUR_SPREADSHEET_ID"
 SPREADSHEET_ENDPOINT = f"https://sheets.googleapis.com/v4/spreadsheets/" \
                        f"{SPREADSHEET_ID}/values/"
 
-
 # Tequila - Flight Search
-TEQUILA_API_KEY = "YOUR_TQUILA_API_KEY"
+TEQUILA_API_KEY = "YOUR_TEQUILA_API_KEY"
 TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com/locations/query"
 TEQUILA_ENDPOINT_FLIGHT = "https://tequila-api.kiwi.com/v2/search"
 
@@ -16,7 +15,7 @@ TEQUILA_ENDPOINT_FLIGHT = "https://tequila-api.kiwi.com/v2/search"
 TWILIO_PHONE = "YOUR_TWILIO_PHONE_NUMBER"
 TWILIO_ACCOUNT_SID = "YOUR_TWILIO_ACCOUNT_SID"
 TWILIO_AUTH_TOKEN = "YOUR_TWILIO_AUTH_TOKEN"
-MY_PHONE = "PPHONE_NUMBER_WHERE_YOU_GET_THE_TEXT"
+MY_PHONE = "PHONE_NUMBER_WHERE_YOU_GET_THE_TEXT"
 
 # Email
 # NOTE: You may need to update mailbox security settings to allow access from insecure apps
